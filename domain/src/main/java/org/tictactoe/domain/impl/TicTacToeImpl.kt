d7366a877b263ab6e16df11dccecc50944391948
2019-DEV-092 <philippe@movify.com>
2019-02-08 18:59:54 +0100

    test and implement rule `Players cannot play on a played position`

--- domain/src/main/java/org/tictactoe/domain/impl/TicTacToeImpl.kt
+++ domain/src/main/java/org/tictactoe/domain/impl/TicTacToeImpl.kt
@@ -1,21 +1,44 @@
 package org.tictactoe.domain.impl
 
 import org.tictactoe.domain.api.*
-import org.tictactoe.domain.api.model.Event
-import org.tictactoe.domain.api.model.Move
-import org.tictactoe.domain.api.model.PlayerX
-import org.tictactoe.domain.api.model.State
+import org.tictactoe.domain.api.model.*
 
 /**
  * Represents the Game
  */
 class TicTacToeImpl : TicTacToe {
 
+    private lateinit var currentState: State
+
     override var onEvent: ((Event) -> Unit)? = null
 
-    override fun reset() :State = State(PlayerX, emptyList(), emptyList())
+    override fun reset() = NEWSTATE.also { currentState = it }
 
     override fun play(move: Move): State {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (currentState.availableMoves.contains(move)) {
+            val nextPlayer = nextPlayer()
+            val previousMoves = listOf(*currentState.previousMoves.toTypedArray(), move)
+            val availableMoves = currentState.availableMoves
+                .filter { it != move }
+                .map { Move(nextPlayer, it.col, it.row) }
+            currentState = State(nextPlayer(), availableMoves, previousMoves)
+        }
+
+        return currentState
     }
+
+    private fun nextPlayer() =
+        when (currentState.currentPlayer) {
+            PlayerX -> PlayerO
+            PlayerO -> PlayerX
+        }
+
+    companion object {
+        private val NEWSTATE: State = State(
+            PlayerX,
+            (0..2).map { col -> (0..2).map { row -> Move(PlayerX, col, row) } }.flatten(),
+            emptyList()
+        )
+    }
+
 }
--- domain/src/test/java/org/tictactoe/domain/TicTacToeTest.kt
+++ domain/src/test/java/org/tictactoe/domain/TicTacToeTest.kt
@@ -1,10 +1,11 @@
 package org.tictactoe.domain
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.tictactoe.domain.api.model.PlayerX
 import org.tictactoe.domain.api.TicTacToe
+import org.tictactoe.domain.api.model.PlayerO
 
 class TicTacToeTest {
 
@@ -22,5 +23,21 @@ class TicTacToeTest {
 
         // test
         assertEquals(PlayerX, state.currentPlayer)
+    }
+
+    @Test
+    fun `Players cannot play on a played position`() {
+        // arrange
+        val newState = sut.reset()
+
+        // act
+        // PlayerX plays a random move
+        val firstMove = newState.availableMoves.random()
+        val state1 = sut.play(firstMove)
+        // PlayerO try to replay PlayerX move
+        val state2 = sut.play(firstMove)
+
+        // test
+        assertEquals(state1, state2)
     }
 }
